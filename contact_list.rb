f749375d9d5ea8de31cff789240c93f47ebd7a5d
Brianne Duffy <duffymbrianne@gmail.com>
2018-10-25 09:14:54 -0600

    better

--- contact_list.rb
+++ contact_list.rb
@@ -1,40 +1,58 @@
 @contacts = []
 
 def menu
-  puts `clear`
+  puts ''
   puts "================================"
-  puts "Hello! Choose an option below!"1
-  puts "1) View All Contacts"
-  puts "2) Create Contact"
+  puts "Hello! Choose an option below!"
+  puts "1) Create Contact"
+  puts "2) View All Contacts"
   puts "3) Exit"
   puts "================================="
-  user_input = gets.strip.downcase
-  user_input.to_i
+  user_input
+end
+
+def user_input
+  case gets.to_i
+  when 1
+    contact_input
+  when 2
+    puts @contacts.join("\n")
+    puts ''
+    puts ''
+    puts "Would you like to return to the menu? Enter yes or no."
+    menu_return
+  when 3
+    puts "Goodbye!"
+    exit
+  else
+    puts "Oops! Try again!"
+    menu
+  end
 end
 
 def contact_input
-  puts "What is the contact name?"
-  contact_name = gets.strip
-  if contact_name.length > 0
-    @contacts << contact_name
-    puts "#{contact_name} Added."
+  puts "What is the contacts first and last name?"
+  name = gets.strip
+  if name.length > 0
+    @contacts << name
+    puts "#{name} was added to your contacts."
+    puts ''
+    puts ''
+    puts "Would you like to return to the menu? Enter yes or no."
+    menu_return
   else
-    puts "Please supply a contact name."
     contact_input
   end
 end
 
-while true
-  case menu
-    when 1
-      puts @contacts.join("\n")
-    when 2
-      contact_input
-    when 3
-      puts "Goodbye!"
-      Exit
-    else
-      puts "Oops! Try again!"
-      menu
-    end
+def menu_return
+  case gets.strip.downcase
+  when 'yes'
+    puts menu
+  else
+    puts 'Goodbye!'
+    exit
+  end
 end
+
+menu
